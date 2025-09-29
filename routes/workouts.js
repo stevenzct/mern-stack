0b5706e89145272cfd5617cb78fb27012974b401
stevenzct <stevencabugos138@gmail.com>
2025-09-29 13:58:23 +0800

    Initial commit

---
+++ routes/workouts.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const Workout = require('../models/workoutModel')
+
+const router = express.Router()
+
+// get all workouts
+router.get('/', (req, res) => {
+    res.json({msg: 'Get all workouts'})
+})
+
+// get a single workout
+router.get('/:id', (req, res) => {
+  res.json({msg: 'GET a single workout'})
+})
+
+// post a new workout
+router.post('/', async (req, res) => {
+    const {title, load, reps} = req.body
+
+    try {
+      const workout = await Workout.create({title, load, reps})
+      res.status(200).json(workout)
+    } catch (error) {
+      res.status(400).json({error: error.message})
+   }
+})
+
+// delete a workout
+router.delete('/:id', (req, res) => {
+    res.json({msg: 'DELETE a workout'})
+})
+
+// update a workout
+router.patch('/:id', (req, res) => {
+    res.json({msg: 'UPDATE a workout'})
+})
+
+module.exports = router
